06874d536f88e7c739ddea37ef585b789d0f0e2a
sebajones <sebastian.jones@outlook.com>
2022-04-22 13:43:07 +0100

    final

--- agentframework.py
+++ agentframework.py
@@ -1,7 +1,9 @@
+# import libraries
 import random
 
+# create Agent class
 class Agent():
-    # make agents
+# initialise Agent class take in data variables
     def __init__(self, environment, agents, y, x):
         self.y = y
         self.x = x
@@ -9,31 +11,35 @@ class Agent():
         self.store = 0
         self.agents = agents
 
+# define function to move agents
     def move(self):
-        if random.random() < 0.5:
-            self.y = (self.y + 1) % 300
-        else:
-            self.y = (self.y - 1) % 300
+        if random.random() < 0.5: # if randomly generated number less than 0.5
+            self.y = (self.y + 1) % 300     # add 1 from y value
+        else:                               # otherwise
+            self.y = (self.y - 1) % 300     # subtract 1 from y value
 
-        if random.random() < 0.5:
-            self.x = (self.x + 1) % 300
-        else:
-            self.x = (self.x - 1) % 300
+        if random.random() < 0.5: # if randomly generated number less than 0.5
+            self.x = (self.x + 1) % 300     # add 1 from x value
+        else:                               # otherwise
+            self.x = (self.x - 1) % 300     # subtract 1 from x value
 
+# define function to share resources with nearby neighbours
     def share_with_neighbours(self, neighbourhood):
-        for agent in self.agents:
-            dist = self.distance_between(agent)
-            if dist <= neighbourhood:
-                sum = self.store + agent.store
-                ave = sum /2
-                self.store = ave
-                agent.store = ave
+        for agent in self.agents:               # loop through agents
+            dist = self.distance_between(agent) # calculate distance between self and other agent
+            if dist <= neighbourhood:           # if distance less than or equal to agent
+                sum = self.store + agent.store  # add self store and other agent store
+                ave = sum /2                    # take average
+                self.store = ave                # assign average value to store value
+                agent.store = ave               # assign average value to store value
                 #print("sharing " + str(dist) + " " + str(ave))
 
+# define function to eat environment
     def eat(self):
-        if self.environment[self.y][self.x] > 10:
-            self.environment[self.y][self.x] -= 10
-            self.store += 10
+        if self.environment[self.y][self.x] > 10:  # if environment xy greater than 10
+            self.environment[self.y][self.x] -= 10 # take 10 from environment xy
+            self.store += 10                       # add 10 to store
 
+# define function to calculate distance between self and another agent
     def distance_between(self, agent):
-        return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
+        return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5 # calculate distance
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 # import libraries
-import random
+#import random
 import operator
 import matplotlib
 import matplotlib.pyplot
@@ -14,23 +14,22 @@ import bs4
 
 # get url and parse data
 r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
-content = r.text
+content = r.text    # create content variable
 soup = bs4.BeautifulSoup(content, 'html.parser')
-# find x and y tags
-td_ys = soup.find_all(attrs={"class" : "y"})
-td_xs = soup.find_all(attrs={"class" : "x"})
+td_ys = soup.find_all(attrs={"class" : "y"})    # find y tags
+td_xs = soup.find_all(attrs={"class" : "x"})    # find y tags
 
 # create empty list for the environment data
 environment = []
 
 # read in environment data and append to the environment list
-with open("in.txt", newline='') as f:
+with open("in.txt", newline='') as f: # open environment data file
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    for row in reader:
-        rowlist = []
-        for value in row:
-            rowlist.append(value)
-        environment.append(rowlist)
+    for row in reader:              # loop through rows
+        rowlist = []                # create empty rowlist list
+        for value in row:           # loop through values
+            rowlist.append(value)   # append to rowlist list
+        environment.append(rowlist) # append to environment list
 
 # set model variable values
 num_of_agents = 100
@@ -60,11 +59,11 @@ def update(frame_number):
     global carry_on
 
     # move agents
-    for j in range(num_of_iterations):
-        for i in range(num_of_agents):
-            agents[i].move()
-            agents[i].eat()
-            agents[i].share_with_neighbours(neighbourhood)
+    for j in range(num_of_iterations):      # loop through num_of_iterations
+        for i in range(num_of_agents):      # loop through agents
+            agents[i].move()                # call move function
+            agents[i].eat()                 # call eat function
+            agents[i].share_with_neighbours(neighbourhood)  # call share with neighbours function
 
     # animation stopping condition
     #while random.random < 0.1:
@@ -76,33 +75,35 @@ def update(frame_number):
     #matplotlib.pyplot.ylim(0, 99)
 
     # show environment surface
-    matplotlib.pyplot.imshow(environment, cmap='terrain')
+    matplotlib.pyplot.imshow(environment, cmap='terrain') # colour surface
     for i in range(num_of_agents):      # loop through agents xy values
         matplotlib.pyplot.scatter(agents[i].x,agents[i].y,      #plot xy
         color='whitesmoke', edgecolors='slategray')     # set colours
 
-# define generator function stops supplying when the condition is met
+# define generator function, stops supplying when the condition is met
 def gen_function(b = [0]):
     a = 0
     global carry_on
-    while (a < 10) & (carry_on) :
+    while (a < 10) & (carry_on) : # loop while a is less than 10
         yield a			# returns control and waits for next call
-        a = a + 1
+        a = a + 1       # add 1 to a
 
+# define run function
 def run():
     animation = matplotlib.animation.FuncAnimation(fig, update,
-    frames=gen_function, repeat=True)
-    canvas.draw()
+    frames=gen_function, repeat=True) # set animation parameters
+    canvas.draw()   # draw canvas
 
-root = tkinter.Tk()
-root.wm_title("Model")
-canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
-canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-menu_bar = tkinter.Menu(root)
-root.config(menu=menu_bar)
+# create Graphical User Interface
+root = tkinter.Tk()         # main window
+root.wm_title("Model")      # set title
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)  # backend and connect with figure
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)            # layout
+menu_bar = tkinter.Menu(root)   # create menu
+root.config(menu=menu_bar)      # configure menu
 model_menu = tkinter.Menu(menu_bar)
-menu_bar.add_cascade(label="Model", menu=model_menu)
-model_menu.add_command(label="Run model", command=run)
+menu_bar.add_cascade(label="Model", menu=model_menu) # create cascade menu
+model_menu.add_command(label="Run model", command=run) # label menu and connect command 'run' function
 
 
-root.mainloop()
+root.mainloop()     # wait for interactions
